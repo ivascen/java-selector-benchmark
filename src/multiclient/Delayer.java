d1757b47ba022bb1b8e7ebc0e015933770d19553
Ivan Valeriani <ivaleriani@pushtechnology.com>
2015-07-05 21:37:49 +0100

    Removing old package

--- src/multiclient/Delayer.java
+++
@@ -1,7 +0,0 @@
-package multiclient;
-
-public interface Delayer {
-
-    long apply(long delay, int clients, int index);
-
-}
--- src/multiclient/NioEventHandler.java
+++
@@ -1,9 +0,0 @@
-package multiclient;
-
-import java.nio.channels.SelectionKey;
-
-public interface NioEventHandler {
-
-    void onKeySelected(SelectionKey key);
-    
-}
